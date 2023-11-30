5437b132a7aeaaaa12cceab77ce5e7be011594a8
BLyman <135188980+Lyman17@users.noreply.github.com>
2023-11-30 11:56:39 -0700

    updated components & added basic styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
+import '../styles/Footer.css';
+
 function Footer() {
     return (
-        <div>
-            <p>Hello, World!</p>
+        <div className="footer">
+            <h2 >Contact Information</h2>
+            <a href="mailto:Brian@UtahMLShomes.com">Brian@UtahMLShomes.com</a>
         </div>
     );
 }
